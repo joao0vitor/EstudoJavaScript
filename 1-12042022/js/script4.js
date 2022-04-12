414766be4e7314f561eb32befe629e9b82a3182e
JOAO VITOR DA SILVA SOUZA <joao.vssouza42@senacsp.edu.br>
2022-04-12 17:34:18 -0300

    colocando script de impar ou par na pagina hmtl

---
+++ 1-12042022/js/script4.js
@@ -0,0 +1,15 @@
+/*
+O script deve receber um número, processar e exibir uma
+mensagem dizendo se o número digitado é par ou inpar
+*/
+var numero = prompt("Digite um número para saber se é impar ou par")
+
+
+if(numero % 2 ==0){
+    alert("Esse número é par");
+}
+
+else{
+    alert("Esse número é impar");
+}
+
